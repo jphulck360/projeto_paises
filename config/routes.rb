d6f4ff0b5909755c88db84fae22720d60de7546f
jphulck360 <jphulck360@hotmail.com>
2017-09-01 17:39:37 -0300

    ATUALIZADO_1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :countries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'countries#index' # Página inicial
 end
